925aeed1ff6574a9e527a92654aeff280978e70f
Alexander372 <aleksandr.shemet@gmail.com>
2020-10-11 22:29:32 +0300

    third

---
+++ src/main/java/Alexander372/SpringInActionTraining/SpringInActionTrainingApplication.java
@@ -0,0 +1,13 @@
+package Alexander372.SpringInActionTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInActionTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Alexander372/SpringInActionTraining/SpringInActionTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package Alexander372.SpringInActionTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
